c6aa52830a16708d8d76da5f12ff4a59c0c248f7
Alexei Godfray <58841622+AlexeiGodfray@users.noreply.github.com>
2024-08-05 11:34:10 -0700

    Main function to serve as loop, and to see if everything is working so far

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import struct
 import textwrap
 
 def main():
-    conn = socket.socket()
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    while True:
+        raw_data, addr = conn.recvfrom(65535)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet Frame:')
+        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
 
 #unpack ethrent frame
 def ethernet_frame(data):
@@ -12,6 +17,8 @@ def ethernet_frame(data):
 
 #Return properly formated MAC address
 def get_mac_addr(bytes_addr):
-    bytes_str = map('{02x}'.format, bytes_addr)
+    bytes_str = map('{:02x}'.format, bytes_addr)
     mac_addr = ':'.join(bytes_str).upper()
     return mac_addr
+
+main()
